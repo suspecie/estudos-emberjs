4cde752f518af38b4316e27091e05a3b3df13fad
Suellyn Specie <sspecie13@gmail.com>
2017-10-11 18:25:20 -0300

    Criando sub rotas e navegações

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  this.route('accounts');
+  this.route('accounts', function() {
+    this.route('view', {path: 'view/:id'});
+  });
 });
 
 export default Router;
