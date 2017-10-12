e0e1cd677c50a75a91690460af31416264e78be4
Suellyn Specie <sspecie13@gmail.com>
2017-10-12 01:59:29 -0300

    Editando e removendo registros

--- app/routes/accounts/edit.js
+++ app/routes/accounts/edit.js
@@ -1,4 +1,21 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
+
+const { inject: { service } } = Ember;
 
 export default Route.extend({
+    account: Ember.inject.service(),
+    actions: {
+        save (model) {
+            this.get('account').update(model.id, model).then( () => {
+                alert('Salvo com sucesso!');
+                let owner = Ember.getOwner(this); //o owner pega o dono save como por exemplo o route.
+                let route = owner.lookup('route:accounts'); //pego a rota accounts
+                route.refresh(); //esse refresh para mostar a listagem atualizada, atualiza o conteúdo da rota pai
+            });
+        }
+    },
+    model (params) {
+        return this.get('account').one(params.id);
+    }
 });
--- app/routes/accounts/remove.js
+++ app/routes/accounts/remove.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
+
+const { inject: { service } } = Ember;
 
 export default Route.extend({
+    account: Ember.inject.service(),
+    model (params) {
+        this.get('account').delete(params.id).then( () => {
+            alert('Excluido com sucesso!');
+            let owner = Ember.getOwner(this); //o owner pega o dono save como por exemplo o route.
+            let route = owner.lookup('route:accounts'); //pego a rota accounts
+            route.refresh(); //esse refresh para mostar a listagem atualizada, atualiza o conteúdo da rota pai
+            this.transitionTo('accounts'); // envia para a rota pai
+        });
+    }
 });
