af85d2db08deaf9cfbce59cc8075e44da5da2303
Suellyn Specie <sspecie13@gmail.com>
2017-10-12 01:48:59 -0300

    Inserindo registros

--- app/routes/accounts/add.js
+++ app/routes/accounts/add.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
+import Ember from 'ember';
+
+const { inject: { service } } = Ember;
 
 export default Route.extend({
+    account: Ember.inject.service(),
+    actions: {
+        save (model) {
+            this.get('account').create(model).then( () => {
+                alert('Salvo com sucesso!');
+                let owner = Ember.getOwner(this); //o owner pega o dono save como por exemplo o route.
+                let route = owner.lookup('route:accounts'); //pego a rota accounts
+                route.refresh(); //esse refresh para mostar a listagem atualizada, atualiza o conteúdo da rota pai
+                this.transitionTo('accounts'); // envia para a rota pai
+            });
+        }
+    },
+    model () {
+        return {};
+    }
 });
--- app/services/account.js
+++ app/services/account.js
@@ -10,12 +10,12 @@ export default Service.extend({
         return this.get('requester').getMethod('accounts/' + id);
     },
     create: function (data) {
-        return this.get('requester').getMethod('accounts/', data);
+        return this.get('requester').postMethod('accounts/', data);
     },
     update: function (id, data) {
-        return this.get('requester').getMethod('accounts/' + id, CDATASection);
+        return this.get('requester').putMethod('accounts/' + id, data);
     },
     delete: function (id) {
-        return this.get('requester').getMethod('accounts/' + id);
+        return this.get('requester').deleteMethod('accounts/' + id);
     }
 });
